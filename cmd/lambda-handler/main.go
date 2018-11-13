5042f72959bfd6fdd3b7b3029668d15349d2209d
mike_neck <jkrt3333@gmail.com>
2018-11-14 01:12:25 +0900

    関数が間違えている

--- cmd/lambda-handler/main.go
+++ cmd/lambda-handler/main.go
@@ -32,7 +32,7 @@ func main() {
 	lambda.Start(Handle)
 }
 
-func Handle(ctx context.Context, req *events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+func Handle(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	log.Println("normal", "request-context:", ctx)
 	log.Println("normal", "request-body:", req.Body)
 	log.Println("normal", "request-header:", req.Headers)
@@ -43,7 +43,7 @@ func Handle(ctx context.Context, req *events.APIGatewayProxyRequest) (*events.AP
 		log.Println("error", "parse request:", "body:", req.Body, "error:", e)
 		err := ErrorResponse{Item: "json", Message: "invalid request"}
 		bytes, _ := json.Marshal(err)
-		return &events.APIGatewayProxyResponse{StatusCode: 400, Body: string(bytes)}, &err
+		return events.APIGatewayProxyResponse{StatusCode: 400, Body: string(bytes)}, &err
 	}
 
 	code := request.Status
@@ -58,17 +58,17 @@ func Handle(ctx context.Context, req *events.APIGatewayProxyRequest) (*events.AP
 		if request.Err {
 			response := ErrorResponse{Item: "user", Message: request.Message}
 			bytes, _ := json.Marshal(response)
-			return &events.APIGatewayProxyResponse{StatusCode: code, Body: string(bytes)}, &response
+			return events.APIGatewayProxyResponse{StatusCode: code, Body: string(bytes)}, &response
 		}
 		response := NormalResponse{Message: request.Message}
 		bytes, _ := json.Marshal(response)
-		return &events.APIGatewayProxyResponse{StatusCode: code, Body: string(bytes)}, nil
+		return events.APIGatewayProxyResponse{StatusCode: code, Body: string(bytes)}, nil
 	}
 
 	response := ErrorResponse{Item: "status-code", Message: "not in list"}
 	bytes, _ := json.Marshal(response)
 	if request.Err {
-		return &events.APIGatewayProxyResponse{StatusCode: http.StatusNotFound, Body: string(bytes)}, &response
+		return events.APIGatewayProxyResponse{StatusCode: http.StatusNotFound, Body: string(bytes)}, &response
 	}
-	return &events.APIGatewayProxyResponse{StatusCode: http.StatusNotFound, Body: string(bytes)}, nil
+	return events.APIGatewayProxyResponse{StatusCode: http.StatusNotFound, Body: string(bytes)}, nil
 }
